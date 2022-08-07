18b08397967c10928f0772d5fd342007263b959d
Abhiram Cherukupalli <cherukupalliabhiram@gmail.com>
2022-08-07 23:41:20 +0530

    edited Naive Metropolis

--- Classical-Ising-Model/Two-Dimensional-Ising-Model/(007) Naive metropolis.py
+++ Classical-Ising-Model/Two-Dimensional-Ising-Model/(007) Naive metropolis.py
@@ -69,4 +69,33 @@ metropolis_steps = 2**11
 T       = np.linspace(1.25, 3.5, 60); 
 E,M,C,X = np.zeros(nt), np.zeros(nt), np.zeros(nt), np.zeros(nt)
 N1  = 1.0/(metropolis_steps*N**2)
-N2 = 1.0/(metropolis_steps*N**2) 
+N2 = 1.0/(metropolis_steps*metropolis_steps*size**2) 
+
+
+for Temppoint in range(60):
+    spinconfig = initial(size)         # initialise
+
+    E0=0
+    E1=0
+    M0=0
+    M1=0
+
+    
+    for i in range(eqSteps):       
+        move(spinconfig, 1.0/T[Temppoint])          
+
+    for i in range(mcSteps):
+        move(spinconfig, 1.0/T[Temppoint])           
+
+        E0 = E0 + Energy(spinconfig)
+        M0 = M0+ Magnetisation(spinconfig)
+        M1 = M1 + Magnetisation(spinconfig)*Magnetisation(spinconfig)
+        E1 = E1 + Energy(spinconfig)*Energy(spinconfig)
+
+
+    # divide by number of sites and iteractions to obtain intensive values    
+    E[Temppoint] = E0
+    M[Temppoint] = 1.0/(metropolis_steps*size**2)*M1
+    C[Temppoint] = (1.0/(metropolis_steps*size**2)*E1 - 1.0/(metropolis_steps*metropolis_steps*size**2) *E0**2)*1.0/(T[Temppoint])**2
+    X[Temppoint] = (1.0/(metropolis_steps*size**2)*M1 - 1.0/(metropolis_steps*metropolis_steps*size**2) *M0**2)*1.0/(T[Temppoint])
+    
