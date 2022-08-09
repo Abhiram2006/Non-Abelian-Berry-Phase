86dd645893b4f9225d99602649c0574ecf3b7818
Abhiram Cherukupalli <cherukupalliabhiram@gmail.com>
2022-08-09 12:59:43 +0530

    added LVC plotting eigenvalues

---
+++ Berry-Phase-In-Various-System/LPQ and LVC/LVC and LPQ plotting.py
@@ -0,0 +1,15 @@
+Es = []
+r1s = np.linspace(-4,4,1000)
+for r1 in r1s:
+    eigenvalues = np.real(np.sort(eig(H_LVC(r1,0,1,2,1,2))[0]))
+    Es.append(eigenvalues)
+Es = np.array(Es)
+plt.figure(figsize=(8,6))
+plt.rcParams['figure.dpi']=2000
+plt.plot(r1s,Es[:,0],label=r'$E_0$')
+plt.plot(r1s,Es[:,1],label=r'$E_1$')
+plt.xlabel(r'$r_1$')
+plt.ylabel('E')
+plt.title('LVC Hamiltonian')
+plt.legend(fontsize=20)
+neaten_plot(plt.gcf())
